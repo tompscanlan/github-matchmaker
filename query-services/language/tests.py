4a5d1fbdd0eef4b3e49ea3bdf4bd1641fecef29a
Tom Scanlan <tscanlan@vmware.com>
2017-08-16 13:39:46 -0500

    enable CORS and fix tests

--- query-services/language/tests.py
+++ query-services/language/tests.py
@@ -7,12 +7,12 @@ from api import *
 class TestCase(unittest.TestCase):
 
     def test_generate_empty_issue_query(self):
-        m = Mock()
-        assert m.generateIssueQuery() == ('{"query": ""}', 200)
+        l = Language()
+        assert l.generateIssueQuery() == ({'query': ''}, 200)
 
     def test_generate_text_issue_query(self):
-        m = Mock()
-        assert m.generateIssueQuery("text") == ('{"query": "text"}', 200)
+        l = Language()
+        assert l.generateIssueQuery("text") == ({'query': 'language:text'}, 200)
 
 
 if __name__ == '__main__':
